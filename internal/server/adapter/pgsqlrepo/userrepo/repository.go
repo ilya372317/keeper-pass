743b5895bc71453a021f4ece390d587b9374af2b
Ilya Otinov <ilya.otinov@gmail.com>
2024-04-16 22:26:48 +0700

    feat: add docker-compose config

--- internal/server/adapter/pgsqlrepo/userrepo/repository.go
+++ internal/server/adapter/pgsqlrepo/userrepo/repository.go
@@ -2,6 +2,7 @@ package userrepo
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ilya372317/pass-keeper/internal/server/domain"
 	"github.com/jmoiron/sqlx"
@@ -16,5 +17,12 @@ func New(db *sqlx.DB) *Repository {
 }
 
 func (r *Repository) GetUserByEmail(ctx context.Context, email string) (domain.User, error) {
-	return domain.User{}, nil
+	user := domain.User{}
+	err := r.db.GetContext(ctx,
+		&user, "SELECT id, email, hashed_password FROM users WHERE email = $1;", email)
+	if err != nil {
+		return domain.User{}, fmt.Errorf("failed get user by email [%s]: %w", email, err)
+	}
+
+	return user, nil
 }
